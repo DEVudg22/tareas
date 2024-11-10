de954e92e9f5790d393be605299752f178daca2a
GulliFerWD <clxl22@hotmail.com>
2024-11-10 16:22:43 -0600

    INICIAL

---
+++ app/tarea/page.jsx
@@ -0,0 +1,3 @@
+export default function TareaPage() {
+  return <h1>TAREA</h1>;
+}
---
+++ app/tareas/page.jsx
@@ -0,0 +1,7 @@
+export default function TareasPage() {
+  return (
+    <div>
+      <h1>TAREAS</h1>
+    </div>
+  );
+}
